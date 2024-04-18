ebdcbfe004d54ff9ed7fffce9bf58399c0537432
Neg00se <gooosb205@gmail.com>
2024-04-19 01:30:20 +0300

    Communal fees repo

---
+++ GasApi/DataAccessLayer/Repositories/CommunalPaymentsRepository.cs
@@ -0,0 +1,51 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories;
+
+public class CommunalPaymentsRepository : ICommunalPaymentsRepository
+{
+	private readonly GasDbContext _context;
+
+	public CommunalPaymentsRepository(GasDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<List<CommunalPayment>> GetAllAsync()
+	{
+		var payments = await _context.CommunalPayments.Include(cp => cp.Rate).ToListAsync();
+		return payments;
+	}
+
+	public async Task<CommunalPayment> GetByIdAsync(int id)
+	{
+		var payment = await _context.CommunalPayments.Include(cp => cp.Rate).FirstOrDefaultAsync(
+			p => p.Id == id);
+		return payment;
+	}
+
+	public async Task AddAsync(CommunalPayment cp)
+	{
+		await _context.AddAsync(cp);
+	}
+
+	public void Update(CommunalPayment cp)
+	{
+		_context.Update(cp);
+	}
+
+	public async Task DeleteByIdAsync(int id)
+	{
+		var cp = await _context.CommunalPayments.FindAsync(id);
+		if (cp is not null)
+		{
+			_context.CommunalPayments.Remove(cp);
+		}
+	}
+}
---
+++ GasApi/DataAccessLayer/Repositories/ICommunalPaymentsRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.Repositories
+{
+	public interface ICommunalPaymentsRepository
+	{
+		Task AddAsync(CommunalPayment cp);
+		Task DeleteByIdAsync(int id);
+		Task<List<CommunalPayment>> GetAllAsync();
+		Task<CommunalPayment> GetByIdAsync(int id);
+		void Update(CommunalPayment cp);
+	}
+}
