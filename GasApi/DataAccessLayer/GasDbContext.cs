bbb9b9fdb166ea5bb76f01e4fe64e06cdc5a5377
Neg00se <gooosb205@gmail.com>
2024-04-19 01:29:54 +0300

    app db context + migrations

--- GasApi/DataAccessLayer/GasDbContext.cs
+++ GasApi/DataAccessLayer/GasDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,7 @@ public class GasDbContext : DbContext
 
 	}
 
+	public DbSet<Rate> Rates { get; set; }
+    public DbSet<CommunalPayment> CommunalPayments { get; set; }
 
 }
---
+++ GasApi/DataAccessLayer/Migrations/20240418213538_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CommunalPayments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    UserCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GasUsed = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ToPay = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Payed = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsPayed = table.Column<bool>(type: "bit", nullable: false),
+                    RateId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommunalPayments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CommunalPayments_Rates_RateId",
+                        column: x => x.RateId,
+                        principalTable: "Rates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunalPayments_RateId",
+                table: "CommunalPayments",
+                column: "RateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommunalPayments");
+
+            migrationBuilder.DropTable(
+                name: "Rates");
+        }
+    }
+}
