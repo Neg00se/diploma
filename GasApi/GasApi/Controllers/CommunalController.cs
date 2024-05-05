a8a91dd437b4728752d5728fa8673202dd3939b3
Neg00se <gooosb205@gmail.com>
2024-05-06 00:53:35 +0300

    Add controllers

---
+++ GasApi/GasApi/Controllers/CommunalController.cs
@@ -0,0 +1,91 @@
+﻿using BusinessLogicLayer.Models;
+using BusinessLogicLayer.Services;
+using DataAccessLayer.IdentityItems;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace GasApi.Controllers;
+
+[Route("api/[controller]")]
+[Authorize]
+[ApiController]
+public class CommunalController : ControllerBase
+{
+	private readonly ICommunalPaymentService _communalService;
+	private readonly UserManager<User> _userManager;
+
+	public CommunalController(ICommunalPaymentService communalService , UserManager<User> userManager)
+	{
+		_communalService = communalService;
+		_userManager = userManager;
+	}
+
+	[HttpGet("toPay")]
+	public async Task<IActionResult> ToPay()
+	{
+		try
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var toPay = await _communalService.ToPayAsync(userId);
+			return Ok(toPay);
+		}
+		catch (Exception ex)
+		{
+
+			return BadRequest(ex.Message);
+		}
+	}
+
+	[HttpGet("get")]
+	public async Task<IActionResult> GetCommunals()
+	{
+		try
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var communals = await _communalService.GetAllAsync(userId);
+			return Ok(communals);
+		}
+		catch (Exception ex)
+		{
+
+			return BadRequest(ex.Message);
+		}
+	}
+
+
+	[HttpPost("pay")]
+	public async Task<IActionResult> Pay(decimal amount)
+	{
+		try
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			await _communalService.PayBillsAsync(amount , userId);
+			return Ok();
+		}
+		catch (Exception ex)
+		{
+
+			return BadRequest(ex.Message);
+		}
+	}
+
+	[HttpPost("addCommunal")]
+	public async Task<IActionResult> AddCommunalPayment(UserIndicatorsModel userIndicators)
+	{
+		try
+		{
+		
+			var user = await _userManager.GetUserAsync(User);
+			await _communalService.AddAsync(userIndicators, user);
+			return Ok();
+		}
+		catch (Exception ex)
+		{
+
+			return BadRequest(ex.Message);
+		}
+	}
+}
--- GasApi/GasApi/Controllers/UserController.cs
+++ GasApi/GasApi/Controllers/UserController.cs
@@ -1,27 +1,39 @@
-﻿using DataAccessLayer.IdentityItems;
+﻿using BusinessLogicLayer.Services;
+using DataAccessLayer.IdentityItems;
+using GasApi.DataSeed;
 using GasApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace GasApi.Controllers
 {
 	[Route("api/[controller]")]
+	[Authorize]
 	[ApiController]
 	public class UserController : ControllerBase
 	{
 		private readonly UserManager<User> _userManager;
-		private readonly SignInManager<User> _signInManager;
+		private readonly IUserService _userService;
+		private readonly ICommunalPaymentService _cpService;
 
-		public UserController(UserManager<User> userManager, SignInManager<User> signInManager)
+		public UserController(UserManager<User> userManager,
+						SignInManager<User> signInManager,
+						IUserService userService,
+						ICommunalPaymentService cpService)
 		{
 			_userManager = userManager;
-			_signInManager = signInManager;
+			_userService = userService;
+			_cpService = cpService;
 		}
 
 		[HttpPost("register")]
+		[AllowAnonymous]
 		public async Task<IActionResult> Register(RegisterModel model)
 		{
+
 			User user = new()
 			{
 				UserName = model.Email,
@@ -32,17 +44,81 @@ namespace GasApi.Controllers
 				Address = model.Address,
 				PhoneNumber = model.Phone
 			};
+			user.RateId = await _userService.GetUserDefaultRandomRateId();
 
 			var result = await _userManager.CreateAsync(user, model.Password);
-			
+
 
 			if (result.Succeeded)
 			{
+				user = await _userManager.FindByNameAsync(user.UserName);
 				await _userManager.AddToRoleAsync(user, "User");
+				await _cpService.AddRandomUserCommunals(user);
 				return Ok("Registration is successfull");
 			}
 
-			return BadRequest("Something went wrong");
+			
+			return BadRequest(result.Errors);
+		}
+
+		
+		[HttpGet("profile")]
+		public async Task<IActionResult> UserProfile()
+		{
+			try
+			{
+				var user = await _userService.GetUserInfo(User);
+				return Ok(user);
+
+			}
+			catch (Exception ex)
+			{
+
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpGet("userRate")]
+		public async Task<IActionResult> UserPlan()
+		{
+			try
+			{
+				var plan =await _userService.GetUserRatePlan(User);
+				return Ok(plan);
+			}
+			catch (Exception ex)
+			{
+
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpPost("changeCreds")]
+		public async Task<IActionResult> ChangeCredentials(ChangeCredentialsModel model)
+		{
+			var user = await _userManager.GetUserAsync(User);
+
+			try
+			{
+				if (!string.IsNullOrWhiteSpace(model.Email))
+				{
+					var token = await _userManager.GenerateChangeEmailTokenAsync(user, model.Email);
+					await _userManager.ChangeEmailAsync(user, model.Email, token);
+					await _userManager.SetUserNameAsync(user, model.Email);
+				}
+
+				if(!string.IsNullOrWhiteSpace(model.NewPassword) && !string.IsNullOrWhiteSpace(model.OldPassword))
+				{
+					await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+				}
+
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+
+				return BadRequest(ex.Message);
+			}
 		}
 
 	}
