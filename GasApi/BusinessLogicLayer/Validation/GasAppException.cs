e4011210e397b1efc855681dad6f683f2f7150d9
Neg00se <gooosb205@gmail.com>
2024-05-06 00:49:09 +0300

    Add Gas Exception

---
+++ GasApi/BusinessLogicLayer/Validation/GasAppException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Validation;
+
+public class GasAppException : Exception
+{
+    public GasAppException()
+    {
+            
+    }
+
+    public GasAppException(string message):base(message)
+    {
+        
+    }
+
+    public GasAppException(string message, Exception inner) : base(message, inner)
+    {
+            
+    }
+}
