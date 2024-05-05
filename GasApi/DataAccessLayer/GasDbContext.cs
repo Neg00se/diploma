83be414684fac4224396724f710e3fd05ce74e1b
Neg00se <gooosb205@gmail.com>
2024-05-06 00:52:17 +0300

    added onModelCreating

--- GasApi/DataAccessLayer/GasDbContext.cs
+++ GasApi/DataAccessLayer/GasDbContext.cs
@@ -16,6 +16,17 @@ public class GasDbContext : DbContext
 	}
 
 	public DbSet<Rate> Rates { get; set; }
-    public DbSet<CommunalPayment> CommunalPayments { get; set; }
+	public DbSet<CommunalPayment> CommunalPayments { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Rate>().HasData(new Rate { Id = 1, Name = "Економ", Price = 7.96m },
+			new Rate { Id = 2, Name = "Фіксований", Price = 8.96m },
+			new Rate { Id = 3, Name = "Преміум", Price = 9.96m });
+
+		modelBuilder.Entity<CommunalPayment>()
+			  .HasIndex(cp => new { cp.StartDate, cp.EndDate , cp.UserId })
+			  .IsUnique();
+	}
 
 }
