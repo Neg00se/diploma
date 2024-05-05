f28042a7d1b273de321a50ea68d9c82df8e5f652
Neg00se <gooosb205@gmail.com>
2024-05-06 00:49:29 +0300

    Add automapper

---
+++ GasApi/BusinessLogicLayer/AutomapperProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.Models;
+using DataAccessLayer.Entities;
+using DataAccessLayer.IdentityItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer;
+
+public class AutomapperProfile : Profile
+{
+	public AutomapperProfile()
+	{
+		CreateMap<CommunalPayment, CommunalInfoModel>()
+		.ForMember(ci => ci.RateName, cp => cp.MapFrom(x => x.Rate.Name))
+		.ForMember(ci => ci.RatePrice, cp => cp.MapFrom(x => x.Rate.Price))
+		//.ForMember(ci => ci.RateId, cp => cp.MapFrom(x => x.RateId))
+		//.ReverseMap()
+		;
+
+		CreateMap<Rate, RatePlanModel>();
+
+		CreateMap<UserIndicatorsModel, CommunalPayment>();
+
+		CreateMap<User, UserModel>().ReverseMap();
+	}
+}
