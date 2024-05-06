353e7cdb8f895e542763e36282f1ba6b91ace8ce
Neg00se <gooosb205@gmail.com>
2024-05-06 18:57:12 +0300

    Added amin controller

---
+++ GasApi/GasApi/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer.IdentityItems;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GasApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize(Roles ="Admin")]
+	public class AdminController : ControllerBase
+	{
+		private readonly UserManager<User> _userManager;
+
+		public AdminController(UserManager<User> userManager)
+        {
+			_userManager = userManager;
+		}
+
+		[HttpGet("getUsers")]
+		public async Task<IActionResult> GetAllUsers()
+		{
+			try
+			{
+				var users = await _userManager.GetUsersInRoleAsync("User");
+				return Ok(users);
+			}
+			catch (Exception ex)
+			{
+
+				return BadRequest(ex.Message);
+			}
+		}
+    }
+}
