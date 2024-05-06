91b602cbf212b912c3c20634fedd52076fe0fa20
Neg00se <gooosb205@gmail.com>
2024-05-06 18:59:44 +0300

    add redux store provider and dayjs localization provider

--- kharkivgas/src/main.jsx
+++ kharkivgas/src/main.jsx
@@ -1,34 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-import Mainreg from './registrationpage/Mainreg'
-import Mainlogin from './loginPage/Mainlogin'
-import UserMainPage from './userInterface/UserMainPage';
-import Error from './errorpage/Error';
-import WelcomePage from './welcomePage/WelcomePage';
-const router=  createBrowserRouter([{
-	path: '/',
-	element: <WelcomePage/>,
-	errorElement: <Error/>
-},
-{
-	path: '/login',
-	element: <Mainlogin/>,
-	
-},
-{
-	path: '/userprofile',
-	element: <UserMainPage/>,
-	
-},
-{
-	path: '/registration',
-	element: <Mainreg/>,
-}])
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import "./index.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Mainlogin/> */}
-<RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
