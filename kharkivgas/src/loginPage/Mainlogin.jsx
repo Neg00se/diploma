464edf72a15e9627adade0a3e921bccf66f99977
Neg00se <gooosb205@gmail.com>
2024-05-06 19:05:46 +0300

    Add logic to login page

--- kharkivgas/src/loginPage/Mainlogin.jsx
+++ kharkivgas/src/loginPage/Mainlogin.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import LoginMainContent from './loginMainContent/LoginMainContent'
-import './mainlogin.css'
+import React from "react";
+import LoginMainContent from "./loginMainContent/LoginMainContent";
+import "./mainlogin.css";
+import { apiSlice } from "../app/api/apiSlice";
+import { useDispatch } from "react-redux";
 export default function Mainlogin() {
+  const dispatch = useDispatch();
+  dispatch(apiSlice.util.resetApiState());
   return (
-	 <>
-	 <LoginMainContent/>
-	 </>
-  )
+    <>
+      <LoginMainContent />
+    </>
+  );
 }
--- kharkivgas/src/loginPage/loginMainContent/LoginMainContent.jsx
+++ kharkivgas/src/loginPage/loginMainContent/LoginMainContent.jsx
@@ -1,11 +1,68 @@
 import React from "react";
 import "./loginmaincontent.css";
-import logo from "../loginImages/logo.png";
-import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import { Link, useNavigate } from "react-router-dom";
 import banner from "../loginImages/login-image.png";
+import { useRef, useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "../../features/auth/authSlice";
+import { useLoginMutation } from "../../features/auth/authApiSlice";
+import { Container, Spinner } from "react-bootstrap";
 
-export default function LoginMainContent() {
-  return (
+const LoginMainContent = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const navigate = useNavigate();
+
+  const [login, { isLoading, isSuccess }] = useLoginMutation();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userData = await login({ email: user, password: pwd }).unwrap();
+      dispatch(setCredentials({ ...userData, user, pwd }));
+      setUser("");
+      setPwd("");
+      navigate("/profile/main");
+    } catch (err) {
+      if (!err?.status) {
+        setErrMsg("No server response");
+      } else if (err?.status == 400) {
+        setErrMsg("Missing username or password");
+      } else if (err?.status === 401) {
+        setErrMsg("Wrong credentials");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  const handleUserInput = (e) => setUser(e.target.value);
+  const handlePwdInput = (e) => setPwd(e.target.value);
+
+  const content = isLoading ? (
+    <Container>
+      <div className="spinner d-flex justify-content-center align-items-center">
+        <h1>Loading &nbsp;</h1>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    </Container>
+  ) : (
     <div className="login-container">
       <div className="left-section">
         <div className="header">
@@ -13,28 +70,46 @@ export default function LoginMainContent() {
             <Link to="/">
               <img src={logo} alt="Kharkiv Gas Logo" className="logo" />
             </Link>
-				<span>Kharkiv gas</span>
+            <span>Kharkiv gas</span>
           </div>
           <div className="signup-nav">
             <span>Немає акаунту?</span>
-				<Link to="/registration">
-				<a href="#!" className="register-button">
+            <Link to="/register" className="register-button">
               Зареєструватися
-            </a>
             </Link>
-            
           </div>
         </div>
 
-        <form className="login-form">
+        <form onSubmit={handleSubmit} className="login-form">
           <h2 className="login-title">Увійти до особистого кабінету</h2>
-          <p className="login-input-hint">Ваш номер телефону або e-mail</p>
-          <input type="text" placeholder="Логін" />
-          <input type="password" placeholder="Пароль" />
-          <div className="checkbox-container">
-            <input type="checkbox" id="agree" />
-            <label htmlFor="agree">Запам'ятати мене</label>
-          </div>
+          <p
+            ref={errRef}
+            className={errMsg ? "text-danger" : "ivisible"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <label htmlFor="username" className="login-input-hint">
+            Ваш e-mail:
+          </label>
+          <input
+            className="inputField"
+            id="username"
+            value={user}
+            onChange={handleUserInput}
+            ref={userRef}
+            type="text"
+            placeholder="Логін"
+            required
+          />
+          <input
+            value={pwd}
+            onChange={handlePwdInput}
+            id="password"
+            type="password"
+            placeholder="Пароль"
+            required
+          />
           <button type="submit" className="login-button">
             Увійти
           </button>
@@ -45,4 +120,8 @@ export default function LoginMainContent() {
       </div>
     </div>
   );
-}
+
+  return content;
+};
+
+export default LoginMainContent;
